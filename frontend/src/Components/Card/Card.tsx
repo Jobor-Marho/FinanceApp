0f5ab58dfa9abab3977ebb94322099b19ff317fa
Marho <joborfrederick0214@gmail.com>
2025-05-20 18:12:55 +0100

    feat: Frontend - Added new Pages & Routes Dir. Also setup react-router-dom for navigating seamlessly through the app

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,37 +1,45 @@
 // Building a Card component in React for handling Stock data
-
-// import './Card.css'
-import { generateImageUrl } from '../../api';
-import AddPortfolio from '../Portfolio/AddPortfolio/AddPortfolio';
+import { Link } from "react-router-dom";
+import { generateImageUrl } from "../../api";
+import AddPortfolio from "../Portfolio/AddPortfolio/AddPortfolio";
 
 // Interface ssimply implies the structure of the props that the component will receive
 // This is a good practice to ensure that the component receives the correct data types
-interface Props  {
-    companyName: string;
-    price: string;
-    ticker:string;
-    description: string;
-    portfolioCreateHandler: (e: React.FormEvent<HTMLFormElement>) => void;
+interface Props {
+  companyName: string;
+  price: string;
+  ticker: string;
+  description: string;
+  portfolioCreateHandler: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
-const Card =  ({companyName, price, description, ticker, portfolioCreateHandler}: Props) => {
- const imageUrl = generateImageUrl(companyName || "Company Name");
+const Card = ({
+  companyName,
+  price,
+  description,
+  ticker,
+  portfolioCreateHandler,
+}: Props) => {
+  const imageUrl = generateImageUrl(companyName || "Company Name");
   return (
     <>
-
-
-
-        <div className="flex flex-col items-center justify-between w-full pt-6 pb-6 ps-10 pe-10 bg-slate-100 rounded-lg md:flex-row">
-            <img src={imageUrl} alt="stockimage" />
-            <h2 className="font-bold text-center text-black md:text-left">{companyName} - {ticker}</h2>
-            <p className="text-black">{price}</p>
-            <p className="font-bold text-black">
-              {description}
-            </p>
-            <AddPortfolio symbol={ticker} onPorfolioCreate={portfolioCreateHandler} />
-        </div>
+      <div className="flex flex-col items-center justify-between w-full pt-6 pb-6 ps-10 pe-10 bg-slate-100 rounded-lg md:flex-row">
+        <img src={imageUrl} alt="stockimage" />
+        <Link
+          to={`/company/${ticker}`}
+          className="font-bold text-center text-black md:text-left"
+        >
+          {companyName} - {ticker}
+        </Link>
+        <p className="text-black">{price}</p>
+        <p className="font-bold text-black">{description}</p>
+        <AddPortfolio
+          symbol={ticker}
+          onPorfolioCreate={portfolioCreateHandler}
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ frontend/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -1,21 +1,30 @@
+import { Link } from "react-router-dom";
 import DeletePortfolio from "../DeletePortfolio/DeletePortfolio";
 
-
-interface  Props {
-    portfolioValue: string;
-    portfolioDeleteHandler: (e: React.FormEvent<HTMLFormElement>) => void;
-    name: string;
+interface Props {
+  portfolioValue: string;
+  portfolioDeleteHandler: (e: React.FormEvent<HTMLFormElement>) => void;
+  name: string;
 }
 
-const CardPortfolio = ({portfolioDeleteHandler, portfolioValue, name}: Props) => {
+const CardPortfolio = ({
+  portfolioDeleteHandler,
+  portfolioValue,
+  name,
+}: Props) => {
   return (
     <>
-    <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
-      <p className="pt-6 text-xl font-bold">{name}</p>
-      <DeletePortfolio portfolioDeleteHandler={portfolioDeleteHandler} portfolioValue={portfolioValue}/>
-    </div>
+      <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
+        <Link to={`/company/${name}`} className="pt-6 text-xl font-bold">
+          {name}
+        </Link>
+        <DeletePortfolio
+          portfolioDeleteHandler={portfolioDeleteHandler}
+          portfolioValue={portfolioValue}
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default CardPortfolio;
