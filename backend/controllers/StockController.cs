ba9e684c2785ea320fe1bb7eaf93b7b10e7f352b
Marho <joborfrederick0214@gmail.com>
2025-08-03 16:16:49 +0100

    update: initialized the stock controller

---
+++ backend/controllers/StockController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using backend.interfaces;
+using backend.models;
+
+namespace backend.controllers
+{
+    public class StockController : ControllerBase
+    {
+        private readonly IStockRepo _stockRepo;
+
+        public StockController(IStockRepo stockRepo)
+        {
+            _stockRepo = stockRepo;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult<Stock>> GetStock(int id)
+        {
+            var stock = await _stockRepo.GetStockByIdAsync(id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult<IEnumerable<Stock>>> GetAllStocks()
+        {
+            var stocks = await _stockRepo.GetAllStocksAsync();
+            return Ok(stocks);
+        }
+
+    }
+}
